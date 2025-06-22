6d562c8ff8b0849c8a834265d8a548ea76cad494
Roman Nikitchuk <roman.nikitchuk@oa.edu.ua>
2025-06-22 18:42:24 +0200

    Лабораторна 16. Nuxt, API, Blog Post View

--- app/Http/Controllers/Api/Blog/PostController.php
+++ app/Http/Controllers/Api/Blog/PostController.php
@@ -3,16 +3,35 @@
 namespace App\Http\Controllers\Api\Blog;
 
 use App\Http\Controllers\Controller;
+use App\Models\BlogPost;
 use Illuminate\Http\Request;
-use App\Models\BlogPost; 
+
 class PostController extends Controller
 {
     public function index()
     {
         $posts = BlogPost::with(['user', 'category'])->get();
+        
         return response()->json([
             'success' => true,
-            'data' => $posts,
+            'data' => $posts
+        ]);
+    }
+
+    public function show($id)
+    {
+        $post = BlogPost::with(['user', 'category'])->find($id);
+        
+        if (!$post) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Пост не знайдено'
+            ], 404);
+        }
+        
+        return response()->json([
+            'success' => true,
+            'data' => $post
         ]);
     }
 }
